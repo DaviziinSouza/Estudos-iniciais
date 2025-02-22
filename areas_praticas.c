b5f95ba22caed3fd2e021360ba9d8d5d80824d3b
DaviziinSouza <davidsouzabarros87@gmail.com>
2025-02-22 23:12:37 +0000

    Estudos teste pra calcular media

---
+++ areas_praticas.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (){
+
+//Programa para calculo de notas
+float nota1, nota2, nota3;
+float media;
+
+printf("|****| MEDIA DE NOTAS |***|\n\n");
+
+printf("Digite sua primeira nota: "  );
+scanf("%f", &nota1);
+
+printf("Digite sua segunda nota: " );
+scanf("%f", &nota2);
+
+printf("Digite sua terceira nota: " );
+scanf("%f", &nota3);
+
+media = (nota1 + nota2 + nota3) / 3;
+
+printf("O resultado da média é: %.1f ", media);
+
+return 0;
+}
